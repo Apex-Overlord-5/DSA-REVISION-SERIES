497efadaac93b0ae60f83852f9313cb1dbb06d57
Apex-Overlord-5 <114309468+Apex-Overlord-5@users.noreply.github.com>
2025-04-21 15:28:31 +0530

    Another One

---
+++ 283 Move Zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right]==0:
+                nums.append(0)
+                nums.remove(0)
+
+# THE ABOVE APPROACH BAD APPROACH
