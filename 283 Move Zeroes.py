6c46dea95c90e8edb56ec5497896659799719cb1
Apex-Overlord-5 <114309468+Apex-Overlord-5@users.noreply.github.com>
2025-04-21 16:40:17 +0530

    new approach

--- 283 Move Zeroes.py
+++ 283 Move Zeroes.py
@@ -8,3 +8,15 @@ class Solution:
                 nums.remove(0)
 
 # THE ABOVE APPROACH IS A BAD APPROACH
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        non0 = 0
+        for zero in range(len(nums)):
+            if nums[zero]!=0:
+                nums[zero] , nums[non0] = nums[non0], nums[zero]
+                non0+=1
+
+        return nums
+        
+            
